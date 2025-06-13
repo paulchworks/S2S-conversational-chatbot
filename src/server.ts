d9c4e3448df5e268d80d6df00f4eb6b249a2d23f
paulchworks <paulchworks@gmail.com>
2025-06-13 23:57:28 +0800

    edited credential source

--- src/server.ts
+++ src/server.ts
@@ -21,11 +21,11 @@ const bedrockClient = new NovaSonicBidirectionalStreamClient({
     },
     clientConfig: {
         region: process.env.AWS_REGION || "us-east-1",
-        //credentials: {
-        //   accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-        //    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
-            //sessionToken: process.env.AWS_SESSION_TOKEN // Optional
-        //}
+        credentials: {
+           accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+           secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+           //sessionToken: process.env.AWS_SESSION_TOKEN // Optional
+        }
     }
 });
 
